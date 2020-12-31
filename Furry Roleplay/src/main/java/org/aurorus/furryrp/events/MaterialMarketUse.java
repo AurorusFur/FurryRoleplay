adfe2fe971e3d46f33aeab660f93a45a5a217a5d
AurorusFur <firelol2004@gmail.com>
2020-12-31 11:50:56 +0100

    No lore items don't send "null lore error"

--- Furry Roleplay/src/main/java/org/aurorus/furryrp/events/MaterialMarketUse.java
+++ Furry Roleplay/src/main/java/org/aurorus/furryrp/events/MaterialMarketUse.java
@@ -38,44 +38,44 @@ public class MaterialMarketUse implements Listener {
             if (!(itemName.equals(ChatColor.RED + "- 100") || itemName.equals(ChatColor.RED + "- 10") || itemName.equals(ChatColor.RED + "- 1") || itemName.contains(ChatColor.WHITE + "Count:") || itemName.equals(ChatColor.GREEN + "+ 1") || itemName.equals(ChatColor.GREEN + "+ 10") || itemName.equals(ChatColor.GREEN + "+ 100") || itemName.equals(ChatColor.RED + "- Page") || itemName.contains(ChatColor.WHITE + "Page:") || itemName.equals(ChatColor.GREEN + "+ Page"))){
                 Sona s = utils.searchPlayerSona(p);
 
-                // FIXME: 29.12.2020 error by clicking no lore item
-                int productPrice = Integer.parseInt(utils.cutColor(item.getLore().get(1)));
+                if(item.hasItemMeta() && item.getItemMeta().hasLore()){
+                    int productPrice = Integer.parseInt(utils.cutColor(item.getLore().get(1)));
 
-                if (s.coins >= productPrice){
-                    int freeSlots = 0;
+                    if (s.coins >= productPrice){
+                        int freeSlots = 0;
 
-                    for (ItemStack i : p.getInventory().getStorageContents()){
-                        if (i == null){
-                            freeSlots++;
+                        for (ItemStack i : p.getInventory().getStorageContents()){
+                            if (i == null){
+                                freeSlots++;
+                            }
                         }
-                    }
 
-                    if (productCount <= 64){
-                        if (freeSlots >= 1){
-                            s.coins = s.coins - productPrice;
-                            p.getInventory().addItem(new ItemStack(item.getType(), productCount));
-                            p.sendMessage(ChatColor.RED + "[- " + productPrice + " " + plugin.coinsName + ChatColor.WHITE + " / " + ChatColor.GREEN + "+ x" + productCount + " " + item.getType() + "]");
+                        if (productCount <= 64){
+                            if (freeSlots >= 1){
+                                s.coins = s.coins - productPrice;
+                                p.getInventory().addItem(new ItemStack(item.getType(), productCount));
+                                p.sendMessage(ChatColor.RED + "[- " + productPrice + " " + plugin.coinsName + ChatColor.WHITE + " / " + ChatColor.GREEN + "+ x" + productCount + " " + item.getType() + "]");
+                            }
+                            else{
+                                p.sendMessage(ChatColor.RED + "You dont have free space in your inventory!");
+                            }
                         }
                         else{
-                            p.sendMessage(ChatColor.RED + "You dont have free space in your inventory!");
+                            if (freeSlots >= Math.ceil(productCount / 64.0)){
+                                s.coins = s.coins - productPrice;
+                                p.getInventory().addItem(new ItemStack(item.getType(), productCount));
+                                p.sendMessage(ChatColor.RED + "[- " + productPrice + " " + plugin.coinsName + ChatColor.WHITE + " / " + ChatColor.GREEN + "+ x" + productCount + " " + item.getType() + "]");
+                            }
+                            else{
+                                p.sendMessage(ChatColor.RED + "You dont have free space in your inventory!");
+                            }
                         }
                     }
                     else{
-                        if (freeSlots >= Math.ceil(productCount / 64)){
-                            s.coins = s.coins - productPrice;
-                            p.getInventory().addItem(new ItemStack(item.getType(), productCount));
-                            p.sendMessage(ChatColor.RED + "[- " + productPrice + " " + plugin.coinsName + ChatColor.WHITE + " / " + ChatColor.GREEN + "+ x" + productCount + " " + item.getType() + "]");
-                        }
-                        else{
-                            p.sendMessage(ChatColor.RED + "You dont have free space in your inventory!");
-                        }
+                        p.sendMessage(ChatColor.RED + "You dont have enough money!");
                     }
                 }
-                else{
-                    p.sendMessage(ChatColor.RED + "You dont have enough money!");
-                }
             }
-
             if (item.getItemMeta().getDisplayName().contains(ChatColor.GREEN + "+ 1")){
                 market.openInv(p, productCount + Integer.parseInt(utils.returnStringInt(utils.cutColor(item.getItemMeta().getDisplayName()))), marketPage);
             }
